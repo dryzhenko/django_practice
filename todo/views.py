4f908aea75b425c4690aa9ebeddef6162c6ef594
Daniil <daniilrizhenko@gmail.com>
2024-11-03 11:17:08 +0200

    modify routing for application

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    current_time = datetime.datetime.now()
+    return HttpResponse(
+        "<html>"
+        "<h1>Hello, world!</h1>"
+        f"<h6>Current time: {current_time}</h6>"
+        "</html>"
+    )
