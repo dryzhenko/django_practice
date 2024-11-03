032c2c37fd1091a3e9a815993f9e632f753925b9
Daniil <daniilrizhenko@gmail.com>
2024-11-03 13:10:45 +0200

    implement function-based views

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
-import datetime
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from todo.forms import TaskForm, TagForm
 from todo.models import Task, Tag
 
 
@@ -18,3 +18,100 @@ def tags(request):
         "tags": tag,
     }
     return render(request, "todo/tags.html", context=context)
+
+
+def task_status_render(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect("todo:index")
+
+
+def add_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("todo:index")
+    else:
+        form = TaskForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "todo/task_form.html", context=context)
+
+
+def add_tag(request):
+    if request.method == "POST":
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("todo:tags")
+    else:
+        form = TagForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "todo/tag_form.html", context=context)
+
+
+def update_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("todo:index")
+    else:
+        form = TaskForm(instance=task)
+
+    context = {
+        "form": form,
+    }
+    return render(request, "todo/task_form.html", context=context)
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == "POST":
+        task.delete()
+        return redirect("todo:index")
+
+    context = {
+        "task": task,
+    }
+    return render(request, "todo/task_confirm_delete.html", context=context)
+
+
+def update_tag(request, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    if request.method == "POST":
+        form = TagForm(request.POST, instance=tag)
+        if form.is_valid():
+            form.save()
+            return redirect("todo:tags")
+    else:
+        form = TagForm(instance=tag)
+
+    context = {
+        "form": form,
+        "tag": tag,
+    }
+    return render(request, "todo/tag_form.html", context=context)
+
+
+def delete_tag(request, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    if request.method == "POST":
+        tag.delete()
+        return redirect("todo:tags")
+
+    context = {
+        "tag": tag,
+    }
+    return render(request, "todo/tag_confirm_delete.html", context=context)
+
